ffd9b13829001a4090d589faf9eb458d1ed4db30
Yang Ge <yangge@broadcom.com>
2014-02-03 17:08:56 -0800

    Added Slicing sample code

---
+++ ch17/17.5/Slicing.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+struct B {
+	int b {66};
+	B() {}
+	B(const B& b) {
+		cout << "I am in B()" << endl;
+	}
+};
+
+struct D : private B {
+	int d {88};
+	D() {}
+	D(const D& d) {
+		cout << "I am in D()" << endl;
+	}
+};
+
+void naive(B *b) {
+	B bb = *b;
+};
+
+int main() {
+	D d;
+	naive(&d);
+	B b = d;
+	cout << b.b << " " << d.d << endl;
+}
