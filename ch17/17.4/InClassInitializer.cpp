f01041aeded8196e835866042ce06d587f659834
Yang Ge <eroicaleo@gmail.com>
2014-11-13 21:06:49 -0800

    updates InClassInitializer.cpp

--- ch17/17.4/InClassInitializer.cpp
+++ ch17/17.4/InClassInitializer.cpp
@@ -1,12 +1,21 @@
 #include <cstdio>
 
 class A {
-	private:
+public:
+	int a = 1;
+};
+
+class AA : public A {
+public:
+	int aa {};
 };
 
 int main() {
 	int i = {};
 	int j {};
 	printf("%d %d\n", i, j);
+	A A;
+	AA AA;
+	printf("A.a = %d, AA.aa = %d, AA.a = %d\n", A.a, AA.aa, AA.a);
 	return 0;
 }
